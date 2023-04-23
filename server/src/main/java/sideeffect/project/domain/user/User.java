b4fdcf527ac46b8905dde7c41261f75f30ae4d1b
tlsrl6427 <tlsr6427@naver.com>
2023-04-23 15:34:35 +0900

    feat: user domain add

--- server/src/main/java/sideeffect/project/domain/user/User.java
+++ server/src/main/java/sideeffect/project/domain/user/User.java
@@ -1,5 +1,30 @@
 package sideeffect.project.domain.user;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
 public class User {
+    @Id
+    @GeneratedValue
+    @Column(name = "user_id")
+    private Long id;
+    @Column(name = "user_name")
+    private String name;
+    private String nickname;
+    private String email;
+    private String position;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
+    private LocalDateTime deleteAt;
+    private String provider;
+    private String imgUrl;
 
 }
