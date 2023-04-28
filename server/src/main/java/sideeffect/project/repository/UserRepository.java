60d99b60eb3ffd8930ad502f16d2abbd201bff9c
tlsrl6427 <tlsr6427@naver.com>
2023-04-28 22:22:13 +0900

    feat: User Repository 구현

--- server/src/main/java/sideeffect/project/repository/UserRepository.java
+++ server/src/main/java/sideeffect/project/repository/UserRepository.java
@@ -1,5 +1,16 @@
 package sideeffect.project.repository;
 
-public class UserRepository {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import sideeffect.project.domain.user.User;
 
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findByEmail(String email);
 }
