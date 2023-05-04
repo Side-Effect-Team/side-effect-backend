14a8c6ce755e7c88e677b965d52fc7c5ebfe1784
tlsrl6427 <tlsr6427@naver.com>
2023-05-04 19:04:03 +0900

    chore: UserController 수정 및 security 적용(test엔 적용x)

--- server/src/main/java/sideeffect/project/controller/UserController.java
+++ server/src/main/java/sideeffect/project/controller/UserController.java
@@ -2,10 +2,11 @@ package sideeffect.project.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import sideeffect.project.dto.user.UserJoinRequest;
+import org.springframework.web.bind.annotation.*;
+import sideeffect.project.domain.user.User;
+import sideeffect.project.dto.user.UserRequest;
+import sideeffect.project.dto.user.UserResponse;
+import sideeffect.project.security.LoginUser;
 import sideeffect.project.service.UserService;
 
 @Slf4j
@@ -16,8 +17,27 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/join")
-    public String join(@RequestBody UserJoinRequest request){
+    public String join(@RequestBody UserRequest request){
+        System.out.println(request.toString());
         userService.join(request);
-        return "success";
+        return "join success";
     }
+
+    @GetMapping("/user/{id}")
+    public UserResponse view(@LoginUser User user, @PathVariable Long id){
+        return userService.findOne(user, id);
+    }
+
+    @PatchMapping("/user/{id}")
+    public String update(@LoginUser User user, @PathVariable Long id, @RequestBody UserRequest request){
+        userService.update(user, id, request);
+        return "update success";
+    }
+    @DeleteMapping("/user/{id}")
+    public String delete(@LoginUser User user, @PathVariable Long id){
+        userService.delete(user, id);
+        return "delete success";
+    }
+
+
 }
