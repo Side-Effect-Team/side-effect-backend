912d97a43700f67f4880ccb69cdb9914e25c202a
tlsrl6427 <tlsr6427@naver.com>
2023-05-04 18:44:38 +0900

    feat: UserDetails(Service) 구현 객체

---
+++ server/src/main/java/sideeffect/project/security/UserDetailsImpl.java
@@ -0,0 +1,58 @@
+package sideeffect.project.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import sideeffect.project.domain.user.User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@AllArgsConstructor
+@Getter
+public class UserDetailsImpl implements UserDetails {
+
+    private User user;
+
+    public static UserDetailsImpl of(User user){
+        return new UserDetailsImpl(user);
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        ArrayList<GrantedAuthority> auth = new ArrayList<>();
+        auth.add(new SimpleGrantedAuthority(user.getUserRoleType().name()));
+        return auth;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
