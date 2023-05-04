7b696d68cbb476cc459d367181e511d5d5e8ba08
tlsrl6427 <tlsr6427@naver.com>
2023-05-04 18:45:24 +0900

    feat: JWT Provider 구현

--- server/src/main/java/sideeffect/project/security/JwtTokenProvider.java
+++ server/src/main/java/sideeffect/project/security/JwtTokenProvider.java
@@ -1,5 +1,70 @@
 package sideeffect.project.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Slf4j
+@Component
 public class JwtTokenProvider {
 
+    @Value("${jwt.secret}")
+    private String secretKey;
+    private Long expired = 1000 * 60 * 60L;
+
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public String getUserName(String token, String secretKey){
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token)
+                .getBody().get("name", String.class);
+    }
+    public boolean isExpired(String token){
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token)
+                .getBody().getExpiration().before(new Date());
+    }
+
+    public String createJwt(Authentication authentication){
+        //권한 가져오기
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        long now = System.currentTimeMillis();
+
+        //access token
+        String accessToken = Jwts.builder()
+                .setSubject(authentication.getName())
+                .claim("auth", authorities)
+                .setExpiration(new Date(now + 1000 * 60 * 60))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+
+        /*
+        //refresh token
+        String refreshToken = Jwts.builder()
+                .setExpiration(new Date(now + 1000 * 60 * 60 * 24))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+        */
+
+        return accessToken;
+    }
+
+    public Authentication getAuthentication(String token){
+        String name = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(name);
+
+        return new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
+    }
 }
