43dc7ba45cce0fefd865b3de1c06c4720d6ff167
tlsrl6427 <tlsr6427@naver.com>
2023-05-07 17:40:05 +0900

    chore: AllowedOriginPatterns로 변경

--- server/src/main/java/sideeffect/project/config/WebMvcConfig.java
+++ server/src/main/java/sideeffect/project/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PATCH", "DELETE")
                 .allowCredentials(true);
     }
