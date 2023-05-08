c7b6c5ad9d6cec67bccbe839b3862b45d17d35ec
kimminsu <xjfcnfw3@naver.com>
2023-05-08 17:55:51 +0900

    feat: Implementing OSIV-related configurations

---
+++ server/src/main/java/sideeffect/project/config/OpenEntityManagerConfig.java
@@ -0,0 +1,18 @@
+package sideeffect.project.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
+
+@Configuration
+public class OpenEntityManagerConfig {
+
+    @Bean
+    public FilterRegistrationBean<OpenEntityManagerInViewFilter> openEntityManagerInViewFilter() {
+        FilterRegistrationBean<OpenEntityManagerInViewFilter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new OpenEntityManagerInViewFilter());
+        filterFilterRegistrationBean.setOrder(Integer.MIN_VALUE);
+        return filterFilterRegistrationBean;
+    }
+}
