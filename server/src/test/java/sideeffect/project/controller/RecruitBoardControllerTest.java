4dbc80f35b6d16ca92d5e40f6f16f85ce9343241
limseonghyun <sksrpf1126@naver.com>
2023-05-10 17:06:30 +0900

    feat: 모집게시판 등록,수정,삭제 API 구현

--- server/src/test/java/sideeffect/project/controller/RecruitBoardControllerTest.java
+++ server/src/test/java/sideeffect/project/controller/RecruitBoardControllerTest.java
@@ -1,46 +1,62 @@
 package sideeffect.project.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import sideeffect.project.common.exception.AuthException;
+import sideeffect.project.common.exception.ErrorCode;
+import sideeffect.project.common.security.WithCustomUser;
+import sideeffect.project.config.WebSecurityConfig;
 import sideeffect.project.domain.position.PositionType;
 import sideeffect.project.domain.recruit.RecruitBoard;
 import sideeffect.project.domain.stack.StackType;
 import sideeffect.project.domain.user.User;
-import sideeffect.project.dto.recruit.BoardPositionResponse;
-import sideeffect.project.dto.recruit.BoardStackResponse;
-import sideeffect.project.dto.recruit.RecruitBoardResponse;
-import sideeffect.project.dto.recruit.RecruitBoardScrollResponse;
+import sideeffect.project.dto.recruit.*;
+import sideeffect.project.security.UserDetailsServiceImpl;
 import sideeffect.project.service.RecruitBoardService;
 
 import java.util.List;
 
 import static org.mockito.BDDMockito.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Import(WebSecurityConfig.class)
+@ComponentScan(basePackages = "sideeffect.project.security")
 @WebMvcTest(RecruitBoardController.class)
 class RecruitBoardControllerTest {
+
+    @MockBean
+    private UserDetailsServiceImpl userDetailsService;
 
     @MockBean
     private RecruitBoardService recruitBoardService;
 
     private MockMvc mvc;
     private User user;
+    private RecruitBoard recruitBoard;
+    private ObjectMapper objectMapper;
 
     @BeforeEach
     void setUp(WebApplicationContext context) {
         mvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity())
                 .build();
 
         user = User.builder()
@@ -48,6 +64,8 @@ class RecruitBoardControllerTest {
                 .email("test@naver.com")
                 .password("qwer1234!")
                 .build();
+
+        objectMapper = new ObjectMapper();
     }
 
     @DisplayName("모집게시글을 조회한다.")
@@ -101,6 +119,70 @@ class RecruitBoardControllerTest {
                 .andDo(print());
 
         verify(recruitBoardService).findRecruitBoards(any());
+    }
+
+    @DisplayName("모집 게시판을 등록한다.")
+    @WithCustomUser
+    @Test
+    void registerRecruitBoard() throws Exception {
+        RecruitBoardRequest request = RecruitBoardRequest.builder().title("모집 게시판1").content("모집합니다1.").build();
+        RecruitBoard recruitBoard = RecruitBoard.builder().id(10L).title("모집 게시판1").contents("모집합니다1.").build();
+        RecruitBoardResponse response = RecruitBoardResponse.of(recruitBoard);
+
+        given(recruitBoardService.register(any(), any())).willReturn(response);
+        mvc.perform(post("/api/recruit-board")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @DisplayName("모집 게시판을 업데이트한다.")
+    @WithCustomUser
+    @Test
+    void updateBoard() throws Exception {
+        RecruitBoardRequest request = RecruitBoardRequest.builder().title("모집 게시판1").content("모집합니다1.").build();
+
+        mvc.perform(patch("/api/recruit-board/1")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("잘못된 유저가 모집 게시판을 업데이트 한다.")
+    @WithCustomUser
+    @Test
+    void updateBoardByNotOwner() throws Exception {
+        RecruitBoardRequest request = RecruitBoardRequest.builder().title("모집 게시판1").content("모집합니다1.").build();
+        doThrow(new AuthException(ErrorCode.RECRUIT_BOARD_UNAUTHORIZED))
+                .when(recruitBoardService).updateRecruitBoard(any(), any(), any());
+        mvc.perform(patch("/api/recruit-board/1")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isForbidden());
+    }
+
+    @DisplayName("모집 게시판을 삭제한다.")
+    @WithCustomUser
+    @Test
+    void deleteBoard() throws Exception {
+        mvc.perform(delete("/api/recruit-board/1")
+                        .with(csrf()))
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("모집 게시판 글 작성자가 아니라면 삭제가 불가능하다.")
+    @WithCustomUser
+    @Test
+    void deleteBoardByNotOwner() throws Exception {
+        doThrow(new AuthException(ErrorCode.RECRUIT_BOARD_UNAUTHORIZED))
+                .when(recruitBoardService).deleteRecruitBoard(any(), any());
+        mvc.perform(delete("/api/recruit-board/1")
+                        .with(csrf()))
+                .andExpect(status().isForbidden());
     }
 
 }
