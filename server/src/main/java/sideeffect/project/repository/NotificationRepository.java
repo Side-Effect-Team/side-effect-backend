76785e0488c9faa7d63e5084d43c3a9b7c8ed353
tlsrl6427 <tlsr6427@naver.com>
2023-05-17 19:34:30 +0900

    feat: Notification Service, Repository 구현

---
+++ server/src/main/java/sideeffect/project/repository/NotificationRepository.java
@@ -0,0 +1,8 @@
+package sideeffect.project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sideeffect.project.domain.notification.Notification;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+}
---
+++ server/src/main/java/sideeffect/project/service/NotificationService.java
@@ -0,0 +1,60 @@
+package sideeffect.project.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sideeffect.project.common.exception.AuthException;
+import sideeffect.project.common.exception.EntityNotFoundException;
+import sideeffect.project.common.exception.ErrorCode;
+import sideeffect.project.domain.notification.Notification;
+import sideeffect.project.domain.user.User;
+import sideeffect.project.dto.notification.NotificationResponse;
+import sideeffect.project.dto.notification.NotificationScrollResponse;
+import sideeffect.project.repository.NotificationRepository;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
+    public List<NotificationResponse> view(User user){
+        List<NotificationResponse> notificationResponses = Collections.emptyList();
+        List<Notification> notifications = user.getNotifications();
+        if(notifications!=null && !notifications.isEmpty()){
+            notificationResponses = notifications.stream()
+                    .map(notification -> NotificationResponse.of(notification))
+                    .collect(Collectors.toList());
+        }
+
+        return notificationResponses;
+    }
+
+    public String watch(User user, Long id){
+        Notification findNotification = notificationRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(ErrorCode.NOTIFICATION_NOT_FOUND));
+        validateOwner(user, findNotification.getUser());
+        findNotification.isWatched(true);
+        return "watched success";
+    }
+
+    public String delete(User user, Long id){
+        Notification findNotification = notificationRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(ErrorCode.NOTIFICATION_NOT_FOUND));
+        validateOwner(user, findNotification.getUser());
+        notificationRepository.deleteById(findNotification.getId());
+        return "delete success";
+    }
+
+    public NotificationScrollResponse scroll(User user, Long lastId){
+
+        return null;
+    }
+    private void validateOwner(User user, User findUser) {
+        if(user.getId() != findUser.getId()) throw new AuthException(ErrorCode.USER_UNAUTHORIZED);
+    }
+}
