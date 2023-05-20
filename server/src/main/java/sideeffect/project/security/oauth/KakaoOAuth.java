9aca5c9548097c3392698432d68abb74db2b9439
tlsrl6427 <tlsr6427@naver.com>
2023-05-20 16:01:48 +0900

    feat: Social 로그인 리다이렉트 빈 구현

---
+++ server/src/main/java/sideeffect/project/security/oauth/KakaoOAuth.java
@@ -0,0 +1,42 @@
+package sideeffect.project.security.oauth;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import sideeffect.project.dto.user.ResponseUserInfo;
+
+@Component
+public class KakaoOAuth implements Oauth{
+
+    private final RestTemplate restTemplate = new RestTemplate();
+    private static final String KAKAO_REQUEST_URL = "https://kapi.kakao.com/v2/user/me";
+    @Override
+    public ResponseUserInfo getUserInfo(String token) {
+        ResponseEntity<String> responseEntity = sendToServer(token);
+        ObjectMapper objectMapper = new ObjectMapper();
+        KakaoUser kakaoUser;
+        try {
+            kakaoUser = objectMapper.readValue(responseEntity.getBody(), KakaoUser.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return ResponseUserInfo.builder()
+                .email(kakaoUser.getKakao_account().getEmail())
+                .imgUrl(kakaoUser.getProperties().getProfile_image())
+                .build();
+    }
+    public ResponseEntity<String> sendToServer(String token) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(headers);
+
+        return restTemplate.exchange(KAKAO_REQUEST_URL, HttpMethod.GET, httpEntity, String.class);
+    }
+
+}
