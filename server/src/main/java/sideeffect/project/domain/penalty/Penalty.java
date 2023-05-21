d6b3cabd0156ff7994d3103bf6880e50c1f7ce66
kimminsu <xjfcnfw3@naver.com>
2023-05-21 20:53:16 +0900

    feat: Implement Penalty

---
+++ server/src/main/java/sideeffect/project/domain/penalty/Penalty.java
@@ -0,0 +1,62 @@
+package sideeffect.project.domain.penalty;
+
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import sideeffect.project.domain.recruit.RecruitBoard;
+import sideeffect.project.domain.user.User;
+
+@Entity
+@Getter
+@Table(name = "penalties",
+    indexes = @Index(name = "board_user_index", columnList = "recruit_board_id, user_id", unique = true)
+)
+@EntityListeners(value = AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Penalty {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recruit_board_id")
+    private RecruitBoard recruitBoard;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    public static Penalty penalize(User user, RecruitBoard recruitBoard) {
+        Penalty penalty = new Penalty();
+        penalty.setUser(user);
+        penalty.setRecruitBoard(recruitBoard);
+        return penalty;
+    }
+
+    private void setUser(User user) {
+        this.user = user;
+        this.user.addPenalty(this);
+    }
+
+    private void setRecruitBoard(RecruitBoard recruitBoard) {
+        this.recruitBoard = recruitBoard;
+        this.recruitBoard.addPenalty(this);
+    }
+}
