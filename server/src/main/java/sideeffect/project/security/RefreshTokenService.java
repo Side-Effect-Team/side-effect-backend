578ea94e82847c55ef05be2e7f7fa24f2a65d8e0
kimminsu <xjfcnfw3@naver.com>
2023-05-22 22:27:28 +0900

    feat: Set up redis

--- server/src/main/java/sideeffect/project/security/RefreshTokenService.java
+++ server/src/main/java/sideeffect/project/security/RefreshTokenService.java
@@ -13,7 +13,7 @@ import sideeffect.project.common.exception.ErrorCode;
 import sideeffect.project.domain.token.RefreshToken;
 import sideeffect.project.domain.user.User;
 import sideeffect.project.domain.user.UserRoleType;
-import sideeffect.project.repository.RefreshTokenRepository;
+import sideeffect.project.redis.RefreshTokenRepository;
 import sideeffect.project.repository.UserRepository;
 
 import java.util.Date;
@@ -32,20 +32,22 @@ public class RefreshTokenService {
     private final UserRepository userRepository;
 
     public String issueAccessToken(String refreshToken){
-        RefreshToken findRefreshToken = refreshTokenRepository.findById(refreshToken).orElseThrow(() -> new EntityNotFoundException(ErrorCode.REFRESH_TOKEN_NOT_FOUND));
-        User findUser = userRepository.findById(findRefreshToken.getUserId()).orElseThrow(() -> new EntityNotFoundException(ErrorCode.USER_NOT_FOUND));
+        RefreshToken findRefreshToken = refreshTokenRepository.findById(refreshToken)
+            .orElseThrow(() -> new EntityNotFoundException(ErrorCode.REFRESH_TOKEN_NOT_FOUND));
+        User user = userRepository.findById(findRefreshToken.getUserId())
+            .orElseThrow(() -> new EntityNotFoundException(ErrorCode.USER_NOT_FOUND));
 
         long now = System.currentTimeMillis();
 
         return Jwts.builder()
-                .setSubject(findUser.getEmail())
+                .setSubject(user.getEmail())
                 .claim("auth", UserRoleType.ROLE_USER)
                 .setExpiration(new Date(now + 1000 * 60 * 60 * 24 * 3))
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
     }
 
-    public String issueRefreshToken(Authentication authentication){
+    public String createRefreshToken(Authentication authentication){
         UUID uuid = UUID.randomUUID();
         User user = (User) authentication.getPrincipal();
         RefreshToken refreshToken = RefreshToken.builder()
@@ -54,5 +56,9 @@ public class RefreshTokenService {
                 .build();
         refreshTokenRepository.save(refreshToken);
         return uuid.toString();
+    }
+
+    public boolean validateRefreshToken(String token) {
+        return  refreshTokenRepository.existsByRefreshToken(token);
     }
 }
