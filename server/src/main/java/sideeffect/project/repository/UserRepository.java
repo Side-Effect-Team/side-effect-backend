5967adba3d71ef1a47b190fb31a5debdaa231643
kimminsu <xjfcnfw3@naver.com>
2023-05-22 23:03:19 +0900

    feat: Apply querydsl to UserRepository

--- server/src/main/java/sideeffect/project/repository/UserRepository.java
+++ server/src/main/java/sideeffect/project/repository/UserRepository.java
@@ -7,8 +7,9 @@ import sideeffect.project.domain.user.ProviderType;
 import sideeffect.project.domain.user.User;
 
 import java.util.Optional;
+import sideeffect.project.repository.user.UserRepositoryCustom;
 
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
     Optional<User> findByEmail(String email);
     Optional<User> findByNickname(String nickname);
     @Query("SELECT u FROM User u " +
