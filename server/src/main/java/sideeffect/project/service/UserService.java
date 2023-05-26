ffa318c02aa40e847f2a882107dba506d9f8506d
tlsrl6427 <tlsr6427@naver.com>
2023-05-26 20:41:17 +0900

    chore: log add

--- server/src/main/java/sideeffect/project/service/UserService.java
+++ server/src/main/java/sideeffect/project/service/UserService.java
@@ -99,6 +99,7 @@ public class UserService {
             String filePath = userUploadService.storeFile(file);
             log.info("filepath: " + filePath);
             user.updateImgUrl(filePath);
+            log.info(user.getImgUrl());
         } catch (IOException e) {
             throw new BaseException(ErrorCode.USER_FILE_UPLOAD_FAILED);
         }
