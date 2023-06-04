a1ee184acf2f80c7877e1e59960bfc0bc7e67a92
kimminsu <xjfcnfw3@naver.com>
2023-06-04 19:10:27 +0900

    feat: Add Oauth2LoginFilter in ConfigFile

--- server/src/main/java/sideeffect/project/config/WebSecurityConfig.java
+++ server/src/main/java/sideeffect/project/config/WebSecurityConfig.java
@@ -2,9 +2,11 @@ package sideeffect.project.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +14,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import sideeffect.project.security.*;
+import sideeffect.project.security.oauth.Oauth2LoginFilter;
+import sideeffect.project.service.OauthService;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -19,10 +23,36 @@ import sideeffect.project.security.*;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig{
 
-    @Value("${jwt.secret}")
-    private String secretKey;
     private final JwtTokenProvider jwtTokenProvider;
     private final RefreshTokenProvider refreshTokenProvider;
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final OauthService oauthService;
+
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    public AuthenticationManager authenticationManager()
+        throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public Oauth2LoginFilter oauth2LoginFilter(OauthService oauthService) throws Exception {
+        Oauth2LoginFilter oauth2LoginFilter = new Oauth2LoginFilter(oauthService);
+        oauth2LoginFilter.setFilterProcessesUrl("/api/social/login");
+        oauth2LoginFilter.setAuthenticationManager(authenticationManager());
+        oauth2LoginFilter.setAuthenticationSuccessHandler(loginSuccessHandler(refreshTokenProvider));
+        return oauth2LoginFilter;
+    }
+
+    @Bean
+    public LoginSuccessHandler loginSuccessHandler(RefreshTokenProvider refreshTokenProvider) {
+        return new LoginSuccessHandler(refreshTokenProvider);
+    }
+
+    @Bean
+    public LoginFailureHandler loginFailureHandler() {
+        return new LoginFailureHandler();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -40,13 +70,14 @@ public class WebSecurityConfig{
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                .addFilterBefore(oauth2LoginFilter(oauthService), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(new JwtFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(new JwtExceptionHandlerFilter(), JwtFilter.class)
+                .addFilterBefore(new SecurityExceptionHandlerFilter(), JwtFilter.class)
                 .formLogin()
                     .loginProcessingUrl("/api/user/login")
                     .usernameParameter("email")
-                    .successHandler(new LoginSuccessHandler(refreshTokenProvider))
-                    .failureHandler(new LoginFailureHandler())
+                    .successHandler(loginSuccessHandler(refreshTokenProvider))
+                    .failureHandler(loginFailureHandler())
                     .and()
                 .build();
     }
